0dfb72acdfa2ba24ad55086ab84f14e99ddc92ac
Alfredo Bejarano <alfredocbejarano@gmail.com>
2022-12-16 17:01:10 -0600

    Update CalculateSuitabilityScoreUseCase.kt

--- app/src/main/java/me/alfredobejarano/sdefleet/domain/CalculateSuitabilityScoreUseCase.kt
+++ app/src/main/java/me/alfredobejarano/sdefleet/domain/CalculateSuitabilityScoreUseCase.kt
@@ -8,6 +8,11 @@ class CalculateSuitabilityScoreUseCase @Inject constructor(
 ) {
     private val vowels = arrayOf('a', 'e', 'i', 'o', 'u')
 
+    /**
+     * Calculate the suitability score (ss) between a Driver and a Shipment.
+     * @param driverName - The name of the driver (Ex. "John Doe")
+     * @param shipment - The address of the shipment (Ex. "123 Oak Street Apt. 21")
+     */
     fun calculateSuitabilityScore(driverName: String, shipment: String): Float {
         val streetAddressSize = extractStreetNameUseCase.extractStreetName(shipment).length
         val isStreetAddressSizeEven = streetAddressSize % 2 == 0
